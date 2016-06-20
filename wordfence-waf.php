4c2c0594900cb7173d70225e77d74439910e4153
rafalpietrzyszyn <rafalpietrzyszyn@gmail.com>
2016-06-20 09:47:27 +0200

    Zmiany w pliku
    Usunąłem komentarz początkowy w zamian dodałem komentarz końcowy.

--- wordfence-waf.php
+++ wordfence-waf.php
@@ -1,8 +1,10 @@
 <?php
-// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
 
 if (file_exists('/home/kolegait/domains/zarnecki.pl/cdv/rpietrzyszyn/wp/wp-content/plugins/wordfence/waf/bootstrap.php')) {
 	define("WFWAF_LOG_PATH", '/home/kolegait/domains/zarnecki.pl/cdv/rpietrzyszyn/wp/wp-content/wflogs/');
 	include_once '/home/kolegait/domains/zarnecki.pl/cdv/rpietrzyszyn/wp/wp-content/plugins/wordfence/waf/bootstrap.php';
 }
 ?>
+
+//sprawdzam czy program wylapuje zmiany w pliku
